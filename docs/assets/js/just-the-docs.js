4f37b7abc75e337557aab4451d1196ce336b5a2d
scaleoutSean <scaleoutsean@users.noreply.github.com>
2020-11-16 17:48:23 +0800

    Use different path

--- docs/assets/js/just-the-docs.js
+++ docs/assets/js/just-the-docs.js
@@ -53,7 +53,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', 'https://scaleoutsean.github.io/solid-rancher/docs/assets/js/search-data.json', true);
+  request.open('GET', 'https://scaleoutsean.github.io/solid-rancher/solid-rancher/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
@@ -430,7 +430,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', 'https://scaleoutsean.github.io/solid-rancher/docs/assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', 'https://scaleoutsean.github.io/solid-rancher/solid-rancher/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Document ready
